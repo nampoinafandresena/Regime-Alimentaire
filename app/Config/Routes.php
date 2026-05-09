390aa2a219663bbcc80e1a5accb84c40781e0519
Harena06 <sahazaharenafeno@gmail.com>
2026-05-09 19:24:33 +0300

    Inscription mandeha

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/inscription/etape-1', 'UserController::inscriptionStep1');
+$routes->post('/inscription/etape-1', 'UserController::storeStep1');
+$routes->get('/inscription/etape-2', 'UserController::inscriptionStep2');
+$routes->post('/inscription/etape-2', 'UserController::storeStep2');
